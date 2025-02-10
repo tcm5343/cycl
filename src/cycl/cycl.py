b05810d21a8f91c2f875186ad4f2ccd08630250b
tcm5343 <48961675+tcm5343@users.noreply.github.com>
2025-02-10 17:41:27 -0600

    Revert "feat: add multi threading when mapping imports (#11)"
    This reverts commit 6fd9e0201fe5a27b20a2c68f0d5b63356f6c3a61.

--- src/cycl/cycl.py
+++ src/cycl/cycl.py
@@ -1,73 +1,25 @@
 from __future__ import annotations
 
-from concurrent.futures import ThreadPoolExecutor
 from logging import getLogger
 from pathlib import Path
-from queue import Queue
-from typing import TYPE_CHECKING
 
-import boto3
 import networkx as nx
-from botocore.config import Config
 
 from cycl.utils.cdk import get_cdk_out_imports
 from cycl.utils.cfn import get_all_exports, get_all_imports, parse_name_from_id
 
-if TYPE_CHECKING:
-    from botocore.client import BaseClient
-
 log = getLogger(__name__)
 
 
-def __get_imports_mapper(export: tuple[str, dict], cfn_client: BaseClient) -> dict:
-    export[1]['ExportingStackName'] = parse_name_from_id(export[1]['ExportingStackId'])
-    export[1].setdefault('ImportingStackNames', []).extend(
-        get_all_imports(export_name=export[1]['Name'], cfn_client=cfn_client)
-    )
-    return {export[0]: export[1]}
+def build_dependency_graph(cdk_out_path: Path | None = None) -> nx.MultiDiGraph:
+    dep_graph = nx.MultiDiGraph()
 
-
-def __map_existing_exports_to_imports(exports: dict) -> dict:
-    res = {}
-    q = Queue()
-
-    max_workers = 10
-    boto_config = Config(
-        retries={
-            'max_attempts': 10,
-            'mode': 'adaptive',
-        },
-        max_pool_connections=max_workers,
-    )
-    cfn_client = boto3.client('cloudformation', config=boto_config)
-
-    for item in exports.items():
-        q.put(item)
-
-    futures = []
-    with ThreadPoolExecutor(max_workers=max_workers) as executor:
-        while not q.empty():
-            futures.append(executor.submit(__get_imports_mapper, export=q.get(), cfn_client=cfn_client))
-
-    for future in futures:
-        err = future.exception()
-        if err:
-            log.error(err)
-        else:
-            res.update(future.result())
-    return res
-
-
-def __get_graph_data(cdk_out_path: Path | None = None) -> dict:
-    """TODO: make this public and test"""
     cdk_out_imports = {}
     if cdk_out_path:
         cdk_out_imports = get_cdk_out_imports(Path(cdk_out_path))
 
     exports = get_all_exports()
-    for export_name, importing_stack_names in cdk_out_imports.items():
-        if export_name in exports:
-            exports[export_name].setdefault('ImportingStackNames', []).extend(importing_stack_names)
+    for export_name in cdk_out_imports:
         if export_name not in exports:
             log.warning(
                 'found an export (%s) which has not been deployed yet about to be imported stack(s): (%s)',
@@ -75,13 +27,10 @@ def __get_graph_data(cdk_out_path: Path | None = None) -> dict:
                 cdk_out_imports[export_name],
             )
 
-    return __map_existing_exports_to_imports(exports=exports)
-
-
-def build_dependency_graph(cdk_out_path: Path | None = None) -> nx.MultiDiGraph:
-    dep_graph = nx.MultiDiGraph()
-    mapped_exports = __get_graph_data(cdk_out_path)
-    for export in mapped_exports.values():
+    for export in exports.values():
+        export['ExportingStackName'] = parse_name_from_id(export['ExportingStackId'])
+        export['ImportingStackNames'] = get_all_imports(export_name=export['Name'])
+        export.setdefault('ImportingStackNames', []).extend(cdk_out_imports.get(export['Name'], []))
         edges = [
             (export['ExportingStackName'], importing_stack_name) for importing_stack_name in export['ImportingStackNames']
         ]
