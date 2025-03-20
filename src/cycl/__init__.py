cb89f3874763e61957ce13b07e7896a9ca06982b
tcm5343 <48961675+tcm5343@users.noreply.github.com>
2025-03-19 22:59:05 -0500

    fix formatting

--- src/cycl/__init__.py
+++ src/cycl/__init__.py
@@ -3,8 +3,8 @@ from importlib.metadata import PackageNotFoundError, version
 from .cycl import build_graph, get_graph_data
 
 try:
-    __version__ = version("cycl")
+    __version__ = version('cycl')
 except PackageNotFoundError:
-    __version__ = "v?"
+    __version__ = 'v?'
 
 print(__version__)
