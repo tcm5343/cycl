789be8bdaacdeca8b65bacccafe5ab205e966dcc
tcm5343 <48961675+tcm5343@users.noreply.github.com>
2025-03-19 22:52:26 -0500

    deploy to test pypi

--- src/cycl/__init__.py
+++ src/cycl/__init__.py
@@ -1,6 +1,10 @@
-# import importlib.metadata
+from importlib.metadata import PackageNotFoundError, version
 
 from .cycl import build_graph, get_graph_data
 
-# TODO: how to resolve?
-# __version__ = importlib.metadata.version(__package__ or __name__)
+try:
+    __version__ = version("cycl")
+except PackageNotFoundError:
+    __version__ = "v?"
+
+print(__version__)
