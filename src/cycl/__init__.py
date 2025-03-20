8f5d8d49176160a355187e708e06d8577ae1918f
tcm5343 <48961675+tcm5343@users.noreply.github.com>
2025-03-20 00:46:20 -0500

    commit it all

--- src/cycl/__init__.py
+++ src/cycl/__init__.py
@@ -1,6 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 from .cycl import build_graph, get_graph_data
+from .utils.tag import format_tag_name as __format_tag_name
 
 try:
     __version__ = version('cycl')
