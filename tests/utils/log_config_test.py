a2279d41fdc7eb7a419912b20181b440f658aad9
tcm5343 <48961675+tcm5343@users.noreply.github.com>
2025-04-08 18:15:59 -0500

    bump reqs

--- tests/utils/log_config_test.py
+++ tests/utils/log_config_test.py
@@ -7,7 +7,8 @@ def test_logging_format(caplog):
     configure_log(logging.INFO)
 
     with caplog.at_level(logging.INFO):
-        logging.info('Test log message')
+        logger = logging.getLogger(__name__)
+        logger.info('Test log message')
 
     assert len(caplog.records) > 0
 
